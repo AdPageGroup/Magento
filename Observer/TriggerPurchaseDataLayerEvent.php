76d9dbea7f67f754d479cb2e0b196e0494950dd1
Victor Kloeppel <vic.kloeppel@gmail.com>
2025-12-05 10:15:57 +0100

    cleaned code and added logging

--- Observer/TriggerPurchaseDataLayerEvent.php
+++ Observer/TriggerPurchaseDataLayerEvent.php
@@ -17,21 +17,15 @@ class TriggerPurchaseDataLayerEvent implements ObserverInterface
     private CheckoutSessionDataProviderInterface $checkoutSessionDataProvider;
     private PurchaseEvent $purchaseEvent;
     private Debugger $debugger;
-    private Config $config;
-    private RequestInterface $request;
 
     public function __construct(
         CheckoutSessionDataProviderInterface $checkoutSessionDataProvider,
         PurchaseEvent $purchaseEvent,
         Debugger $debugger,
-        Config $config,
-        RequestInterface $request
     ) {
         $this->checkoutSessionDataProvider = $checkoutSessionDataProvider;
         $this->purchaseEvent = $purchaseEvent;
         $this->debugger = $debugger;
-        $this->config = $config;
-        $this->request = $request;
     }
 
     public function execute(Observer $observer)
@@ -39,45 +33,9 @@ class TriggerPurchaseDataLayerEvent implements ObserverInterface
         /** @var OrderInterface $order */
         $order = $observer->getData('order');
 
-        $pageCheckEnabled = $this->config->isPurchaseEventPageCheckEnabled();
-        $this->debugger->debug('TriggerPurchaseDataLayerEvent::execute(): page_check_enabled: ' . ($pageCheckEnabled ? 'true' : 'false'));
-
-        if ($pageCheckEnabled) {
-            if (!$this->isCheckoutPage()) {
-                $this->debugger->debug('TriggerPurchaseDataLayerEvent::execute(): Not on an allowed checkout page, skipping purchase event');
-                return;
-            }
-            $this->debugger->debug('TriggerPurchaseDataLayerEvent::execute(): On allowed checkout page, proceeding with purchase event');
-        } else {
-            $this->debugger->debug('TriggerPurchaseDataLayerEvent::execute(): Page check disabled - purchase event will fire from any page');
-        }
-
         $this->checkoutSessionDataProvider->add(
             'purchase_event',
             $this->purchaseEvent->setOrder($order)->get()
         );
-    }
-
-    /**
-     * Check if the current request is on a checkout page
-     *
-     * @return bool
-     */
-    private function isCheckoutPage(): bool
-    {
-        $fullActionName = $this->request->getFullActionName();
-        $moduleName = $this->request->getModuleName();
-        
-        // Check if we're in the checkout module
-        if ($moduleName !== 'checkout') {
-            return false;
-        }
-        
-        $allowedActions = $this->config->getPurchaseEventAllowedCheckoutActions();
-        
-        $this->debugger->debug('TriggerPurchaseDataLayerEvent::isCheckoutPage(): Allowed actions: ' . implode(', ', $allowedActions));
-        $this->debugger->debug('TriggerPurchaseDataLayerEvent::isCheckoutPage(): Current action: ' . $fullActionName);
-        
-        return in_array($fullActionName, $allowedActions, true);
     }
 }
