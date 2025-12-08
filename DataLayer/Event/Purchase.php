850ecf96398bb712a73bbd16add6317fa7e2612e
Jeroen Frenken <jeroen@frostup.com>
2025-12-08 08:43:08 +0100

    Added user_data to purchase event

--- DataLayer/Event/Purchase.php
+++ DataLayer/Event/Purchase.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Tagging\GTM\DataLayer\Event;
 
@@ -42,7 +44,44 @@ class Purchase implements EventInterface
                 'shipping' => $this->priceFormatter->format((float)$order->getShippingInclTax()),
                 'coupon' => $order->getCouponCode(),
                 'items' => $this->orderItems->setOrder($order)->get()
-            ]
+            ],
+            'user_data' => $this->getUserData($order)
+        ];
+    }
+
+    /**
+     * @param OrderInterface $order
+     * @return array
+     */
+    private function getUserData(OrderInterface $order): array
+    {
+        $billingAddress = $order->getBillingAddress();
+        $shippingAddress = $order->getShippingAddress();
+
+        return [
+            'customer_id' => $order->getCustomerId() ?? '',
+            'billing_first_name' => $billingAddress ? $billingAddress->getFirstname() ?? '' : '',
+            'billing_last_name' => $billingAddress ? $billingAddress->getLastname() ?? '' : '',
+            'billing_address' => $billingAddress && $billingAddress->getStreet() ? $billingAddress->getStreet()[0] ?? '' : '',
+            'billing_postcode' => $billingAddress ? $billingAddress->getPostcode() ?? '' : '',
+            'billing_country' => $billingAddress ? $billingAddress->getCountryId() ?? '' : '',
+            'billing_state' => $billingAddress ? $billingAddress->getRegion() ?? '' : '',
+            'billing_city' => $billingAddress ? $billingAddress->getCity() ?? '' : '',
+            'billing_email' => $billingAddress ? $billingAddress->getEmail() ?? '' : '',
+            'billing_phone' => $billingAddress ? $billingAddress->getTelephone() ?? '' : '',
+            'shipping_first_name' => $shippingAddress ? $shippingAddress->getFirstname() ?? '' : '',
+            'shipping_last_name' => $shippingAddress ? $shippingAddress->getLastname() ?? '' : '',
+            'shipping_company' => $shippingAddress ? $shippingAddress->getCompany() ?? '' : '',
+            'shipping_address' => $shippingAddress && $shippingAddress->getStreet() ? $shippingAddress->getStreet()[0] ?? '' : '',
+            'shipping_postcode' => $shippingAddress ? $shippingAddress->getPostcode() ?? '' : '',
+            'shipping_country' => $shippingAddress ? $shippingAddress->getCountryId() ?? '' : '',
+            'shipping_state' => $shippingAddress ? $shippingAddress->getRegion() ?? '' : '',
+            'shipping_city' => $shippingAddress ? $shippingAddress->getCity() ?? '' : '',
+            'shipping_phone' => $shippingAddress ? $shippingAddress->getTelephone() ?? '' : '',
+            'email' => $order->getCustomerEmail() ?? '',
+            'first_name' => $order->getCustomerFirstname() ?? '',
+            'last_name' => $order->getCustomerLastname() ?? '',
+            'new_customer' => $order->getCustomerIsGuest() ? 'true' : 'false'
         ];
     }
 
